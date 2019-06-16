fff63ccdc0fed27ad1f5db14f41071890dfe59a9
Tyler Glisson <Tyler@tglisson.com>
2019-06-16 14:31:49 -0400

    Add scrolling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,17 +4,20 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { thumbnail_image, title, artist } = album;
+    const { image, title, artist } = album;
     return (
         <Card>
             <CardSection>
-                <View>
-                    <Image source={{ uri: thumbnail_image }} />
+                <View style={styles.thumbnailContainerStyle}>
+                    <Image style={styles.thumbnailStyle} source={{ uri: image }} />
                 </View>
                 <View style={styles.headerContentStyles}>
-                    <Text>{title}</Text>
+                    <Text style={styles.headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image style={styles.imageStyle} source={{ uri: image}} />
             </CardSection>
         </Card>
     );
@@ -24,6 +27,24 @@ const styles = {
     headerContentStyles: {
         flexDirect: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
