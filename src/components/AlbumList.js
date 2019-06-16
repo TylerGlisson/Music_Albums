cf43c88d77701f177af2156c0bd77734285c28bc
Tyler Glisson <Tyler@tglisson.com>
2019-06-15 21:35:46 -0400

    Add axios dependency

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+import console = require('console');
 
 class AlbumList extends Component {
     componentWillMount() {
-        console.log('Component will mount!!!');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response))    
     }
 
     render() {    
